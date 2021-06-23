2cdc93234a7de99997dade14b219934f83f3f8d6
pol.bachelin@epitech.eu <pol.bachelin@epitech.eu>
2021-06-23 19:14:37 +0200

    feat(tests/): Added test dir with CMake to link Boost.Test and wrote first Boost test

---
+++ tests/allocator_tests.cpp
@@ -0,0 +1,17 @@
+/*
+** Pol bachelin
+** redCodeEngine
+** File description:
+** allocator_tests
+*/
+
+#define BOOST_TEST_MODULE Allocator testcases
+#include <boost/test/included/unit_test.hpp>
+#include "Allocator.hpp"
+
+BOOST_AUTO_TEST_CASE(simple_test) {
+    BOOST_CHECK_EQUAL(2 + 2, 4);
+}
+
+BOOST_AUTO_TEST_CASE(alloc) {
+}
